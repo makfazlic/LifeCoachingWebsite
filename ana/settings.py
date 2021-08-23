781b831fd8042abacacbfcfd25f95f1c8aef4ad4
Mak Fazlic <59539406+makfazlic@users.noreply.github.com>
2021-08-23 19:14:41 +0200

    Update settings.py

--- ana/settings.py
+++ ana/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3((twp980c*&ud(!979k&j$iq_u4^sc%2my=b0$m01&clxbas*'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
